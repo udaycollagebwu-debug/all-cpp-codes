4d3191693d67d2d176bd58359071a6a8d7f89efd
Uday sankar <udaycollagebwu@gmail.com>
2026-06-01 16:47:38 +0530

    the basic variable

---
+++ variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    char b = 'u';
+    float c = 3.134;
+    cout <<"this is a:"<<a<<endl;
+    cout << "this is b:" << b << endl;
+    cout << "this is c:" << c << endl;
+    return 0;
+}
