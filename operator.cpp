cc35f0e3086aedcd70e34a775016aac30876939c
Uday sankar <udaycollagebwu@gmail.com>
2026-06-02 07:20:37 +0530

    the code and the logic bhind the code

--- operator.cpp
+++ operator.cpp
@@ -1,8 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int mian(){
-    int a,b;
+int mian()
+{
+    int a, b;
     char op;
-    
+    float result;
+    cout << "Enter the two numbers with a space : ";
+    cin >> a;
+    cin >> b;
+    cout << "Enter the operation(+,-,*,/) that you want to do: ";
+    cin >> op;
+    switch (op)
+    {
+    case '+':
+        result = a + b;
+        break;
+    case '-':
+        result = a - b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    case '/':
+        result = a / b;
+        break;
+    default:
+        cout << "You enter a rong operation!!!!!!!";
+    }
+    cout << "The ans (result) is :" << result << endl;
     return 0;
 }
