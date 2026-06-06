ade4962d9a41084b9b83ecc60112ae03534beb99
Uday sankar <udaycollagebwu@gmail.com>
2026-06-06 15:34:03 +0530

    the simpale code has been uploded

---
+++ simple.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num;
+cout<<"In this code I am going to display wather a number is a odd or even ."<<endl;
+cout<<"Enter a number :";
+cin>>num;
+if(num%2 == 0){
+    cout<<"The number that you enterd is a even number .";  // ??----> this line is checking that the number is devided by 2 is equalto 0 or not .?
+                                                              // if the number is 0 then the number is even or the number is odd .
+}
+else{
+    cout<<"The number is odd number .";//lats go and check is working or not ..?
+
+}
+    return 0;
+}
