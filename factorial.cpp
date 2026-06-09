5d32737cab4221e7e9f205d71f48191ec0d15b2b
Uday sankar <udaycollagebwu@gmail.com>
2026-06-09 07:50:33 +0530

    the logic and the code is don

---
+++ factorial.cpp
@@ -0,0 +1,14 @@
+// Print the factorial of a number using a loop.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number, facto = 1;
+    cout << "Enter the number :";
+    cin >> number;
+    for (int i = 1; i <= number; i++)
+    {
+        facto *= i;
+    }
+    cout<<"The factorial of the number "<<number<<" is :"<<facto<<endl;
+}
