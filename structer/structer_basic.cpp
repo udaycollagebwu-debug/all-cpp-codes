45bce28e93f10a2ea9b6415a9cc2be301b697209
Uday sankar <udaycollagebwu@gmail.com>
2026-07-30 17:38:42 +0530

    basic code

---
+++ structer/structer_basic.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct student
+{
+    int stu_id; // we cant initialize any variables inside the structure
+    char name[50];
+    float cgpa;
+};
+int mian()
+{
+    student s1;
+    s1.stu_id = 418;
+    cin >> s1.stu_id;
+    strcpy(s1.name, "uday samkar singha");
+    s1.cgpa = 7.9;
+    cin >> s1.cgpa;
+    cout << "Name :" << s1.name << endl;
+    cout << "Student ID :" << s1.stu_id << endl;
+    cout << "CGPA :" << s1.cgpa << endl;
+    return 0;
+}
