384a52c56bec74b87feff64d8c62d3edcba6b6b3
Uday sankar <udaycollagebwu@gmail.com>
2026-07-30 17:48:42 +0530

    basic code change add a coment line

--- name_input.cpp
+++ name_input.cpp
@@ -4,7 +4,7 @@ int main()
 {
     string name;
     cout << "Enter your name :";
-    getline(cin, name);
+    getline(cin, name);  //take input with the space 
     cout << "The name you enterd is : " << name << endl;
     return 0;
 }
